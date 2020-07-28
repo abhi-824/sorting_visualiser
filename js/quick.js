d5ada73b6822b9e13bccfd010198e73877eedc53
Abhinandan Sharma <abhi.moudgil15@gmail.com>
2020-07-28 21:08:47 +0530

    initial commit

---
+++ js/quick.js
@@ -0,0 +1,34 @@
+
+function Quick_sort(low,high)
+{
+    if(low<high)
+    {
+        let pi=partition(low,high)
+        Quick_sort(low,pi-1);
+        Quick_sort(pi+1,high);
+    }
+}
+function partition(low,high)
+{
+    let pivot=div_heights[high];
+    update_div(low,div_heights[low],"yellow");//Color update
+    let i=low-1;
+    for(let j=low;j<=high-1;j++)
+    {
+        if(div_heights[j]<=pivot)
+        {
+            i++;
+            update_div(i,div_heights[i],"red")
+            update_div(j,div_heights[j],"red")
+            let temp=div_heights[i]
+            div_heights[i]=div_heights[j]
+            div_heights[j]=temp
+            update_div(i,div_heights[i],"teal")
+            update_div(j,div_heights[j],"teal")
+        }
+    }
+    let temp=div_heights[i+1]
+    div_heights[i+1]=div_heights[high]
+    div_heights[high]=temp;
+    return(i+1);
+}
